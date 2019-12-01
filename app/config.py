0573022ade2b5a86fb0ec7ea46fa8d1ea208f9ec
Tomas Bedrich <tbedrich@akamai.com>
2019-12-01 21:37:04 +0100

    Add production config

--- app/config.py
+++ app/config.py
@@ -1,8 +1,10 @@
 from llconfig import Config
+from llconfig.converters import bool_like
 
 c = config = Config()
 c.init('SENDGRID_API_KEY', str, None)
 c.init('MONGO_CONNECTION_STRING', str)
 c.init('CONTACT_FORM_RECEPIENT', str, 'ja@tbedrich.cz')
 c.init('CONTACT_FORM_SUBJECT', str, 'Chmelovar.cz - dotaz')
+c.init('FORCE_HTTPS', bool_like, False)
 c.load()
--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from starlette.requests import Request
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 from pydantic import EmailStr
+from config import config
 
 from services import record_subscribe_request, submit_contact_form
 
@@ -10,6 +11,16 @@ app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
+
+
+if config['FORCE_HTTPS']:
+    @app.middleware("http")
+    async def force_https(request: Request, call_next):
+        # fake HTTPS request scope because of following bug:
+        # https://github.com/encode/uvicorn/issues/505
+        request.scope["scheme"] = "https"
+        response = await call_next(request)
+        return response
 
 
 @app.get("/")

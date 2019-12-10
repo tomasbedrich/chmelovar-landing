4d87790e58c8c99bdc28359e828bb270621e69ce
Tomas Bedrich <tbedrich@akamai.com>
2019-12-10 09:13:27 +0100

    Add error handling, sentry

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM tiangolo/uvicorn-gunicorn-fastapi:python3.7-alpine3.8
 
-RUN pip install llconfig jinja2 aiofiles email-validator python-multipart sendgrid motor
-
-ENV WEB_CONCURRENCY 1
+RUN pip install llconfig jinja2 aiofiles email-validator python-multipart sendgrid motor sentry-sdk==0.13.5
 
 COPY ./app /app
--- app/main.py
+++ app/main.py
@@ -1,18 +1,35 @@
 from fastapi import FastAPI, Form
 from starlette.requests import Request
+from starlette.responses import PlainTextResponse
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
+from starlette.exceptions import HTTPException
 from pydantic import EmailStr
-from config import config
 
+import sentry_sdk
+
+from config import config
 from services import record_subscribe_request, submit_contact_form
 
-app = FastAPI()
+# basics
+app_cnf = {
+    "title": "Chmelovar",
+    "version": "1.0.0",
+}
 
+# disable docs
+if config["DISABLE_DOCS"]:
+    app_cnf["redoc_url"] = None
+    app_cnf["docs_url"] = None
+
+# APP
+app = FastAPI(**app_cnf)
+
+# static files + templates
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-
+# HTTPS on production
 if config['FORCE_HTTPS']:
     @app.middleware("http")
     async def force_https(request: Request, call_next):
@@ -21,6 +38,16 @@ if config['FORCE_HTTPS']:
         request.scope["scheme"] = "https"
         response = await call_next(request)
         return response
+
+
+@app.on_event("startup")
+async def register_sentry():
+    sentry_sdk.init(**config.get_namespace("SENTRY_"))
+
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request, exc):
+    return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
 
 
 @app.get("/")
